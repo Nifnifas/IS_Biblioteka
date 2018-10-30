10a47cc1f5e3fbbe213dbe3538e4cbefd7de88b4
Nifnifas <31838340+Nifnifas@users.noreply.github.com>
2018-10-30 23:59:23 +0200

    Update Renginiu_modulis/renginiuKalendorius.php

---
+++ Renginiu_modulis/renginiuKalendorius.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Bibliotekos informacinė sistema</title>
+</head>
+
+<body>
+    <a href="atsijungimas.php">Atsijungti</a><br/>
+    <a href="paskyrosRedagavimas.php">Redaguoti paskyrą</a><br/>
+    <center>
+        <h1>Bibliotekos informacinė sistema</h1>
+        <table border="1" cellpadding="10">
+            <tr align="center">
+            	Direktoriui:<a href="registruotiRengini.php">Registruoti naują renginį</a><br/>
+            	Klientams:<a href="rezervuotuRenginiuKalendorius.php">Rezervuoti renginiai</a><br/>
+                <p></p>
+                ČIA BUS RODOMAS VISŲ RENGINIŲ SĄRAŠAS (KALENDORIUS)
+                <td><a href="renginioInformacija.php">Renginys1</a></td>
+            	<td>Renginio data, laikas</td>
+            </tr>
+        </table>
+        <br>
+        <div class="container" style="background-color:#f1f1f1">
+            <button onclick="location.href='index.php'" type="button">Grįžti</button>
+        </div>
+    </center>
+</body>
+
+</html>
