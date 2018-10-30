59228040345cc2f7fdf2fafe51357ccf518a79d1
Nifnifas <31838340+Nifnifas@users.noreply.github.com>
2018-10-30 20:10:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+<title>Bibliotekos informacinė sistema</title>
+</head>
+<body>
+<a href="">Atsijungti</a><br/>
+<a href="">Redaguoti paskyrą</a><br/>
+ 
+<center>
+<h1>Bibliotekos informacinė sistema</h1>
+ 
+<table border="1" cellpadding="10">
+<tr align="center">
+<td><a href="">Knygų sąrašas</a></td>
+<td><a href="">Ataskaitų sąrašas</a></td>
+<td>Darbuotojams:<br/><a href="">Skolų sąrašas</a><br/>
+Klientams:<br/><a href="">Skolos langas</a></td>
+<td>Klientams:<br/><a href="">Knygų rezervacijų sąrašas</a></td>
+<td>Darbuotojams:<br/><a href="">Sutarčių sąrašas</a></td>
+<td>Darbuotojams:<br/><a href="">Darbuotojų sąrašas</a></td>
+<td><a href="">Renginių kalendorius</a></td>
+</tr>
+</table>
+</center>
+ 
+</body>
 </html>
